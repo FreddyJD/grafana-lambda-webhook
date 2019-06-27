730db5eafaef43acc29e6f6599cb86128dfc2aa3
freddy.diaz <freddy.diaz@therigy.com>
2019-06-27 16:42:44 -0400

    Initial commit 2

--- handler.js
+++ handler.js
@@ -7,20 +7,18 @@ const {
 module.exports.trigger = async (event) => {
   const data = JSON.parse(event.body)
 
-  console.log('🔥 💁‍')
-  console.log(data.state)
-
-
   const tplink = await login(process.env.tplink_user, process.env.tplink_password);
   await tplink.getDeviceList();
+
   if (data.state === 'ok' || data.state === 'paused' || data.state === 'pending') {
     await tplink.getHS100(process.env.tplink_device_name).powerOff();
 
     return {
       statusCode: 200
     }
+
   } else {
-    await tplink.getHS100(process.env.tplink_device_name).toggle();
+    await tplink.getHS100(process.env.tplink_device_name).powerOn();
     return {
       statusCode: 200
     }
